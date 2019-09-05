d56b15d055a4dd49c67fff04c1b1838c1c2dfd02
Stefan <stefan.gerl@tum.de>
2019-09-05 19:17:36 +0200

    some small tools for label reconstruction

---
+++ prep/utils/ves_merge_label.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 17:09:24 2019
+
+@author: stefan
+"""
+import nibabel as nib
+import numpy as np
+
+
+file = '/home/stefan/PYTHON/HQDatasetVesselAnnot/vessels/R_20190605163439_HQ0003_th_edit.nii.gz'
+
+
+file_handle = nib.load(file)
+label = file_handle.get_fdata()
+label = label.astype(np.uint8)
+
+
+# merge label 1 and 2
+print(np.amax(label))
+label = label.astype(np.bool).astype(np.uint8)
+print(np.amax(label))
+
+
+img = nib.Nifti1Image(label, np.eye(4))
+        
+file = file.replace('.nii.gz','')
+file = file + '_merge.nii.gz'
+nib.save(img, file)
+
+
+
