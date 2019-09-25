2a2860ae3abfb029addb44c8febc1b462ee658d6
Stefan <stefan.gerl@tum.de>
2019-09-25 08:11:35 +0200

    adapt calc_metrics

--- vesnet/unit_tests.py
+++ vesnet/unit_tests.py
@@ -25,7 +25,7 @@ from dataloader import DropBlue, ToTensor, to_numpy
 from patch_handling import get_patches, get_volume
 
 
-os.environ["CUDA_VISIBLE_DEVICES"]='7'
+os.environ["CUDA_VISIBLE_DEVICES"]='5'
 
 # helper functions
 def _dice(x, y):
@@ -67,22 +67,22 @@ class TestCalcMetrics(unittest.TestCase):
         target = copy.deepcopy(pred)
         skel = copy.deepcopy(pred)
 
-        metrics = calc_metrics(pred, target, skel)
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
         dice = _dice(pred, target)
-        self.assertEqual(dice, metrics['dice'])
+        self.assertEqual(dice, dice__)
 
         # two pixel vs one pixel
         target[0, 0, 0, 0, 1] = 1
-        metrics = calc_metrics(pred, target, skel)
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
         dice = _dice(pred, target)
-        self.assertTrue(isclose(dice, metrics['dice']))
+        self.assertTrue(isclose(dice, dice__))
         
         # no overlap, zero
         target[0, 0, 0, 0, 0] = 0
         target[0, 0, 0, 0, 1] = 1
-        metrics = calc_metrics(pred, target, skel)
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
         dice = _dice(pred, target)
-        self.assertEqual(dice, metrics['dice'])
+        self.assertEqual(dice, dice__)
 
     def test_dice_batch_size_2(self):
 
@@ -94,22 +94,22 @@ class TestCalcMetrics(unittest.TestCase):
         target = copy.deepcopy(pred)
         skel = copy.deepcopy(pred)
 
-        metrics = calc_metrics(pred, target, skel)
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
         dice = _dice(pred, target)
-        self.assertEqual(dice, metrics['dice'])
+        self.assertEqual(dice, dice__)
 
         # 1 pixel vs 3 pixel
         target[:, 0, 0, 0, 1] = 1
-        metrics = calc_metrics(pred, target, skel)
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
         dice = _dice(pred, target)
-        self.assertTrue(isclose(dice, metrics['dice']))
+        self.assertTrue(isclose(dice, dice__))
         
         # no overlap, zero
         target[:, 0, 0, 0, :] = 0
         target[1, 0, 0, 0, 0] = 1
-        metrics = calc_metrics(pred, target, skel)
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
         dice = _dice(pred, target)
-        self.assertEqual(dice, metrics['dice'])
+        self.assertEqual(dice, dice__)
 
     def test_cl_score_batch_size_1(self):
         # complete overlap, cl score = 1
@@ -119,26 +119,26 @@ class TestCalcMetrics(unittest.TestCase):
         target = copy.deepcopy(pred)
         skel = copy.deepcopy(pred)
 
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(1., metrics['cl_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(1., cl_score)
 
         #skeleton enclosed
         pred[0, 0, :, :, :] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(1., metrics['cl_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(1., cl_score)
 
         # skeleton outside
         pred[:] = 0
         pred[0, 0, 0, 0, 1] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(0., metrics['cl_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(0., cl_score)
 
         # pixel enclosed 2 pixel outside
         # cl_score 0.333
         skel[0, 0, 0, 0, :] = 1
         skel[0, 0, 0, 1, 0] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertTrue(isclose(1/3, metrics['cl_score']))
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertTrue(isclose(1/3, cl_score))
 
     def test_cl_score_batch_size_2(self):
          # complete overlap, cl score = 1
@@ -148,26 +148,26 @@ class TestCalcMetrics(unittest.TestCase):
         target = copy.deepcopy(pred)
         skel = copy.deepcopy(pred)
         
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(1., metrics['cl_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(1., cl_score)
 
         #skeleton enclosed
         pred[0, 0, :, :, :] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(1., metrics['cl_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(1., cl_score)
 
         # skeleton outside
         pred[:] = 0
         pred[:, 0, 0, 0, 1] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(0., metrics['cl_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(0., cl_score)
 
         # 2 pixel enclosed 6 pixel outside
         # cl_score 1/3
         skel[:, 0, 0, 0, :] = 1
         skel[:, 0, 0, 1, 0] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertTrue(isclose(1/3, metrics['cl_score']))
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertTrue(isclose(1/3, cl_score))
 
     def test_out_score_batch_size_1(self):
         
@@ -179,8 +179,8 @@ class TestCalcMetrics(unittest.TestCase):
         
         # same, outside score = 1
         pred = copy.deepcopy(target)
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(1., metrics['out_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(1., out_score)
     
         # pred completely outside 
         pred = torch.zeros((1, 1, 7, 7, 7), dtype=torch.float32)
@@ -188,15 +188,15 @@ class TestCalcMetrics(unittest.TestCase):
         pred[0, 0, 0, 0, 0] = 1
         pred[0, 0, 0, 0, 6] = 1
         pred[0, 0, 0, 6, 0] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(0., metrics['out_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(0., out_score)
 
         # pred partially inside
         pred = torch.zeros((1, 1, 7, 7, 7), dtype=torch.float32)
         pred = pred.cuda()
         pred[0, 0, 0, 0, 5:7] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(0.5, metrics['out_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(0.5, out_score)
 
     def test_out_score_batch_size_2(self):
         # hull encludes everything but not corner elements, eg [0,0,0,0,0]
@@ -207,8 +207,8 @@ class TestCalcMetrics(unittest.TestCase):
         
         # same, outside score = 1
         pred = copy.deepcopy(target)
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(1., metrics['out_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(1., out_score)
     
         # pred completely outside 
         pred = torch.zeros((2, 1, 7, 7, 7), dtype=torch.float32)
@@ -216,15 +216,15 @@ class TestCalcMetrics(unittest.TestCase):
         pred[0, 0, 0, 0, 0] = 1
         pred[0, 0, 0, 0, 6] = 1
         pred[0, 0, 0, 6, 0] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(0., metrics['out_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(0., out_score)
 
         # pred partially inside
         pred = torch.zeros((2, 1, 7, 7, 7), dtype=torch.float32)
         pred = pred.cuda()
         pred[0, 0, 0, 0, 5:7] = 1
-        metrics = calc_metrics(pred, target, skel)
-        self.assertEqual(0.5, metrics['out_score'])
+        cl_score, out_score, dice__ = calc_metrics(pred, target, skel)
+        self.assertEqual(0.5, out_score)
 
 # dataloader
 class TestDataloaderDataAugmentation(unittest.TestCase):
