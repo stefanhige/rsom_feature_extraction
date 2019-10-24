e8598cee05ae2ad533a9f8b019fc497eac101d7a
Stefan <stefan.gerl@tum.de>
2019-10-24 21:15:08 +0200

    trying a lot of tweaks to get some visualization

--- visualization/pytorch-3d-seg-visual/src/gradcam.py
+++ visualization/pytorch-3d-seg-visual/src/gradcam.py
@@ -6,6 +6,8 @@ Created on Thu Oct 26 11:06:51 2017
 import sys
 from PIL import Image
 import numpy as np
+import nibabel as nib
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -59,34 +61,132 @@ class GradCam():
         conv_output, model_output = self.extractor.forward_pass(input_image)
         
         
-        if target_class is None:
-            target_class = np.argmax(model_output.data.numpy())
+        #if target_class is None:
+        #    target_class = np.argmax(model_output.data.numpy())
+        print(target_class)
         # Target for backprop
         one_hot_output = torch.zeros(tuple(model_output.shape), dtype=torch.float32)
         one_hot_output[0][0][target_class] = 1
+        #one_hot_output[0,0,30:33,14,40] = 1
+        print(one_hot_output.sum())
+                
         # Zero grads
         self.model.layers.zero_grad()
         # Backward pass with specified target
         model_output.backward(gradient=one_hot_output, retain_graph=True)
         # Get hooked gradients
-        guided_gradients = self.extractor.gradients.data.numpy()[0,0, ...]
+        guided_gradients = self.extractor.gradients.data.numpy()[0, ...]
+        
+        print('guided_gradients shape:', guided_gradients.shape)
+        
+        # Relu
+        #guided_gradients = np.maximum(guided_gradients, 0)
+        
+        #img = nib.Nifti1Image(guided_gradients[0,...], np.eye(4))
+        #nib.save(img, 'gg0_.nii.gz')
+        #img = nib.Nifti1Image(guided_gradients[1,...], np.eye(4))
+        #nib.save(img, 'gg1_.nii.gz')
+        #img = nib.Nifti1Image(guided_gradients[2,...], np.eye(4))
+        #return guided_gradients[2, ...]
+        #nib.save(img, 'gg2_.nii.gz')
+        #img = nib.Nifti1Image(guided_gradients[3,...], np.eye(4))
+        #nib.save(img, 'gg3.nii.gz')
+        
+        img = nib.Nifti1Image(np.pad(model_output.detach().numpy().squeeze(),7,mode='constant'), np.eye(4))
+        nib.save(img, 'output.nii.gz')
+        
         # Get convolution outputs
-        # TODO:
-        # works till here,
-        # what is target used for??
-        target = conv_output.data.numpy()[0,0, ...]
+        # these are the "forward activation maps"
+        target = conv_output.data.numpy()[0, ...]
+        
+        img = nib.Nifti1Image(np.pad(target[31, ...],6,mode='constant'), np.eye(4))
+        nib.save(img, 'target31.nii.gz')
+        img = nib.Nifti1Image(np.pad(target[32, ...],6,mode='constant'), np.eye(4))
+        nib.save(img, 'target32.nii.gz')
         # Get weights from gradients
-        weights = np.mean(guided_gradients, axis=(1, 2))  # Take averages for each gradient
+        # average over spatial dimensions
+        # first dimension are feature maps
+        # TODO: average only over "valid" area
+        
+        #location activation map
+#        lam = guided_gradients * target
+#        #relu
+#        lam[lam<0] = 0
+#        
+#        img = nib.Nifti1Image(lam[0, ...], np.eye(4))
+#        nib.save(img, 'lam0.nii.gz')
+#        img = nib.Nifti1Image(lam[1, ...], np.eye(4))
+#        nib.save(img, 'lam1.nii.gz')
+#        
+#        lam = lam / np.amax(lam)
+#        lam = np.mean(lam, axis=0)
+#        img = nib.Nifti1Image(lam, np.eye(4))
+#        nib.save(img, 'lam_mean.nii.gz')
+        
+        
+        
+        
+        ofs = 1
+        weights = np.mean(guided_gradients[:,
+                                           target_class[0]-ofs:target_class[0]+ofs,
+                                           target_class[1]-ofs:target_class[1]+ofs,
+                                           target_class[2]-ofs:target_class[2]+ofs
+                                           ], axis=(1, 2, 3))  # Take averages for each gradient
+        print(weights)
+        print(np.argmax(weights))
+        print(weights[31], weights[32])
+        
+        # add targets with positive weights
+        real_weight = self.model.layers[4].weight.detach().numpy().squeeze()
+        
+        flag = 0
+        for i in range(len(weights)):
+            f = real_weight[i]
+            if real_weight[i] < 0:
+                if not flag:
+                    target_sum_p = -f*target[i,...]
+                    flag = 1
+                else:
+                    target_sum_p += -f*target[i,...]
+                    
+        flag = 0
+        for i in range(len(weights)):
+            f = real_weight[i]
+            if real_weight[i] > 0:
+                if not flag:
+                    target_sum_n = f*target[i,...]
+                    flag = 1
+                else:
+                    target_sum_n += f*target[i,...]
+                    
+        img = nib.Nifti1Image(np.pad(target_sum_n*target_sum_p,6,mode='constant'), np.eye(4))
+        nib.save(img, 'target_sum.nii.gz')
+                    
+                    
+        
         # Create empty numpy array for cam
+#        target = target[:,
+#                        target_class[0]-5:target_class[0]+5,
+#                        target_class[1]-5:target_class[1]+5,
+#                        target_class[2]-5:target_class[2]+5,
+#                        ]
+        
         cam = np.ones(target.shape[1:], dtype=np.float32)
         # Multiply each weight with its conv output and then, sum
         for i, w in enumerate(weights):
-            cam += w * target[i, :, :]
+            cam[target_class[0]-ofs:target_class[0]+ofs,
+                target_class[1]-ofs:target_class[1]+ofs,
+                target_class[2]-ofs:target_class[2]+ofs
+                ] += w * target[i,
+                                target_class[0]-ofs:target_class[0]+ofs,
+                                target_class[1]-ofs:target_class[1]+ofs,
+                                target_class[2]-ofs:target_class[2]+ofs
+                                ]
         cam = np.maximum(cam, 0)
         cam = (cam - np.min(cam)) / (np.max(cam) - np.min(cam))  # Normalize between 0-1
         cam = np.uint8(cam * 255)  # Scale between 0-255 to visualize
-        cam = np.uint8(Image.fromarray(cam).resize((input_image.shape[2],
-                       input_image.shape[3]), Image.ANTIALIAS))/255
+        
+        #cam = np.uint8(Image.fromarray(cam), Image.ANTIALIAS))/255
         # ^ I am extremely unhappy with this line. Originally resizing was done in cv2 which
         # supports resizing numpy matrices with antialiasing, however,
         # when I moved the repository to PIL, this option was out of the window.
@@ -97,32 +197,99 @@ class GradCam():
 
 
 
-target_class = (50, 50, 50) # coordinates
-# load pretrained model
-model_dir = '/home/stefan/data/vesnet/out/191017-00-rt_+backg_bce_gn/mod191017-00.pt'
-model = DeepVesselNet(groupnorm=True) # default settings with group norm
-model.load_state_dict(torch.load(model_dir))
-model.eval()
+target_class_ = [(30, 14, 40),
+                 (31, 14, 40),
+                 (32, 14, 40)]
+                     # coordinates
+target_class_ = [target_class_[0]]
 
-# load rsom images
-# use standard dataset, this is the easiest
-data_dir = '/home/stefan/data/vesnet/annotatedDataset/train'
-
-dataset = RSOMVesselDataset(data_dir,
-                            divs=(1,1,1), 
-                            offset=(6,6,6),
-                            transform=transforms.Compose([
-                                    DropBlue(),
-                                    ToTensor()]))
-img = dataset[0]['data']
-img = img.unsqueeze(0)
-img = img[:, :, :100, :100, :100]
+for i, target_class in enumerate(target_class_):
+    # load pretrained model
+    model_dir = '/home/stefan/data/vesnet/out/191017-00-rt_+backg_bce_gn/mod191017-00.pt'
+    model = DeepVesselNet(groupnorm=True) # default settings with group norm
+    model.load_state_dict(torch.load(model_dir))
+    model.eval()
+    
+    # load rsom images
+    # use standard dataset, this is the easiest
+    data_dir = '/home/stefan/data/vesnet/annotatedDataset/train'
+    
+    dataset = RSOMVesselDataset(data_dir,
+                                divs=(1,1,1), 
+                                offset=(6,6,6),
+                                transform=transforms.Compose([
+                                        DropBlue(),
+                                        ToTensor()]))
+    img = dataset[0]['data']
+    print(dataset[0]['meta']['filename'])
+    img = img.unsqueeze(0)
+    print(img.shape)
+    #img = img[:, :, :99, :100, :101]
+    img = img[:, :, 0:100, :100, :]
+    
+    # Vm is a 4-d numpy array, with the last dim holding RGB
+    inp = img.numpy().copy()
+    inp = np.concatenate((inp,np.zeros((inp.shape[0], 1, inp.shape[2], inp.shape[3], inp.shape[4]))),axis=1)
+    inp = inp.squeeze()
+    inp = np.moveaxis(inp, 0, -1)
+    inp = np.ascontiguousarray(inp)
+    print(inp.shape)
+    print(inp.dtype)
+    shape_3d = inp.shape[0:3]
+    rgb_dtype = np.dtype([('R', 'u1'), ('G', 'u1'), ('B', 'u1')])
+    inp = inp.astype(np.uint8)
+    inp = inp.view(rgb_dtype)
+    inp = inp.reshape(shape_3d)
+    inp = nib.Nifti1Image(inp, np.eye(4))
+    nib.save(inp, 'input.nii.gz')
+    
+        
+        
+    # Grad cam
+    grad_cam = GradCam(model, target_layer=3)
+    # Generate cam mask
+    #cam = grad_cam.generate_cam(img, target_class)
+    
+    if i == 0:
+        gg = grad_cam.generate_cam(img, target_class)
+    else:
+        gg += grad_cam.generate_cam(img, target_class)
     
     
-# Grad cam
-grad_cam = GradCam(model, target_layer=1)
-# Generate cam mask
-cam = grad_cam.generate_cam(img, target_class)
-# Save mask
-save_class_activation_images(original_image, cam, file_name_to_export)
-print('Grad cam completed')
+    # Save mask
+    #save_class_activation_images(original_image, cam, file_name_to_export)
+    
+#    img = nib.Nifti1Image(cam, np.eye(4))
+#    nib.save(img, 'grad_cam_test.nii.gz')
+    
+    print('Grad cam completed')
+
+
+img = nib.Nifti1Image(gg, np.eye(4))
+nib.save(img, 'lin_test.nii.gz')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
